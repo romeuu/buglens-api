54d3935b26288f493a6f606ed81d1d7aa7ef113a
Sergio Romeu <sergioromagr@gmail.com>
2025-06-09 17:27:32 +0200

    feat: Added migrations for scans and plugins table

---
+++ database/migrations/2025_06_09_105258_create_scans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plugin_id')->constrained()->onDelete('cascade');
+            $table->string('scanner');
+            $table->json('result')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scans');
+    }
+};
