a05d997842d8fe568db0e421d868ad3e4caed36f
Sergio Romeu <sergioromagr@gmail.com>
2025-09-18 19:20:09 +0000

    feat: Added scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\CheckVersions;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command(CheckVersions::class)->daily();
+Schedule::command(DownloadUpdates::class)->cron('0 1 * * 1,3,5,0');
